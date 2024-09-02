f4989da9d849795cb9948cec34db94a4a9146178
n-underwood <nathanaelu@gmail.com>
2024-09-02 15:19:32 +0100

    Update myConnector.js

--- myConnector.js
+++ myConnector.js
@@ -33,10 +33,30 @@
 
     myConnector.getData = function(table, doneCallback) {
         var symbol = tableau.connectionData;
-        fetch(`/getData?symbol=${symbol}`)
-            .then(response => response.json())
-            .then(data => {
-                table.appendRows(data);
+        var apiUrl = `https://query1.finance.yahoo.com/v7/finance/download/${symbol}?period1=0&period2=9999999999&interval=1d&events=history&includeAdjustedClose=true`;
+
+        fetch(apiUrl)
+            .then(response => response.text())
+            .then(csv => {
+                var lines = csv.split('\n');
+                var tableData = [];
+
+                for (var i = 1; i < lines.length; i++) {
+                    var fields = lines[i].split(',');
+
+                    if (fields.length === 7) {
+                        tableData.push({
+                            "date": fields[0],
+                            "open": parseFloat(fields[1]),
+                            "high": parseFloat(fields[2]),
+                            "low": parseFloat(fields[3]),
+                            "close": parseFloat(fields[4]),
+                            "volume": parseInt(fields[6])
+                        });
+                    }
+                }
+
+                table.appendRows(tableData);
                 doneCallback();
             });
     };
@@ -48,5 +68,3 @@ function fetchData() {
     var symbol = document.getElementById("symbol").value.trim();
     tableau.connectionData = symbol;
     tableau.connectionName = "Yahoo Finance Data for " + symbol;
-    tableau.submit();
-}
